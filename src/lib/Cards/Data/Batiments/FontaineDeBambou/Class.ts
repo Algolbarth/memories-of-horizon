11b6a1c7af94701af93f6f3d4a63bce3affd9228
Algolbarth <paul.poirier@live.fr>
2025-08-20 14:34:59 +0200

    feat: add "Fontaine de bambou" card

---
+++ src/lib/Cards/Data/Batiments/FontaineDeBambou/Class.ts
@@ -0,0 +1,23 @@
+import type { System } from '../../../../System/Class';
+import { Batiment } from '../../../Class/Batiment';
+import Text from './Text.svelte';
+
+export class FontaineDeBambou extends Batiment {
+    name = "Fontaine de bambou";
+
+    constructor(system: System) {
+        super(system);
+
+        this.init([["Or", 25], ["Eau", 25]]);
+        this.stat("Vie").base = 20;
+        this.stat("Vie").current = 20;
+
+        this.text = Text;
+    };
+
+    startStepEffect = function () {
+        if (this.zone.name == "Terrain") {
+            this.owner.getCard("Écoulement").add("Main");
+        }
+    };
+}
---
+++ src/lib/Cards/Data/Batiments/FontaineDeBambou/Text.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import CardInfo from "../../../Utils/Info.svelte";
+	import type { System } from "../../../../System/Class";
+	import type { Card } from "../../../Class";
+
+	export let system: System;
+	// svelte-ignore export_let_unused
+	export let card: Card;
+</script>
+
+Au début de la phase de préparation : crée <CardInfo bind:system name={"Écoulement"} /> dans la main.
