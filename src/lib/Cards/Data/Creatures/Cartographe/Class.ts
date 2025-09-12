335c5aa2d8fe228c8528273edf767c952f5ddd11
Algolbarth <paul.poirier@live.fr>
2025-09-12 23:07:37 +0200

    feat: add "Cartographe" card

---
+++ src/lib/Cards/Data/Creatures/Cartographe/Class.ts
@@ -0,0 +1,25 @@
+import type { System } from '../../../../System/Class';
+import { Creature } from '../../../Class/Creature';
+import Text from './Text.svelte';
+
+export class Cartographe extends Creature {
+    name = "Cartographe";
+
+    constructor(system: System) {
+        super(system);
+
+        this.init([["Or", 45]]);
+        this.familles.base.push("Humain");
+
+        this.stat("Constitution").init(5);
+        this.stat("Force").init(5);
+
+        this.text = Text;
+    };
+
+    startStepEffect = () => {
+        if (this.zone.name == "Terrain") {
+            this.owner.getCard("Carte de la région").add("Réserve");
+        }
+    };
+}
