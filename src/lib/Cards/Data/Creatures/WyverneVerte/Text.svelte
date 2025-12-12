c2fd1bf236101b3d33f51f0dcbdc92659db0b2d6
Algolbarth <paul.poirier@live.fr>
2025-12-12 01:57:00 +0100

    fix: switch element for "Wyverne rouge" and "Wyverne marine" cards

--- src/lib/Cards/Data/Creatures/WyverneVerte/Text.svelte
+++ src/lib/Cards/Data/Creatures/WyverneVerte/Text.svelte
@@ -8,4 +8,4 @@
 	export let card: Card;
 </script>
 
-Quand une carte alliée d'élément Feu est posée : diminue son coût de 5.
+Quand une carte alliée d'élément Végétal est posée : diminue son coût de 5.
